4ab7a729b1a8d2a74bc4730edb96c297512c0c65
1Dimas3 <87761444+Garnaga5713@users.noreply.github.com>
2022-07-23 11:21:11 +0300

    about

---
+++ About_Project.cmd
@@ -0,0 +1,5 @@
+HYS HACKATHON 2022
+This project was create to make your life a little bit easy.
+When someone want to create CV he must search dozens of sites to find it for free.
+Ta-dam!!! Congratulations you find it.
+Is free app which step by step take information from you by questions that actually need to create CV in .pdf
